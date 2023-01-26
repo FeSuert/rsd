d405cbc9cc20d437bc47c33d2e69512554e0c5ba
aleksey <azhlbn@gmail.com>
2023-01-26 20:57:11 +1000

    upd

--- chain/src/Superfluid.sol
+++ chain/src/Superfluid.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.10;
 
-import "@openzeppelin/contract/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /// @title lil superfluid
 /// @author Miguel Piedrafita
