3464391925301b68312266a6ed3cce5f30837b89
aleksey <azhlbn@gmail.com>
2023-01-27 16:56:42 +1000

    upd

--- chain/src/Safe.sol
+++ chain/src/Safe.sol
@@ -1,143 +1,171 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract SuperSafe {
-	error InvalidSignatures();
-	error ExecutionFailed();
+contract Safe {
+    error InvalidSignatures();
+    error ExecutionFailed();
 
-	event QuorumUpdated(uint256 newQuorum);
-	event Executed(address target, uint256 value, bytes payload);
-	event SignerUpdated(address indexed signer, bool shouldTrust);
+    event QuorumUpdated(uint256 newQuorum);
+    event Executed(address target, uint256 value, bytes payload);
+    event SignerUpdated(address indexed signer, bool shouldTrust);
 
-	struct Signature {
-		uint8 v;
-		bytes32 r;
-		bytes32 s;
-	}
+    struct Signature {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+    }
 
-	uint256 public nonce = 1;
-	uint256 public quorum;
-	bytes32 public immutable domainSeparator;
-	mapping(address => bool) public isSigner;
-	bytes32 public constant QUORUM_HASH =
-		keccak256('UpdateQuorum(uint256 newQuorum,uint256 nonce)');
-	bytes32 public constant SIGNER_HASH =
-		keccak256('UpdateSigner(address signer,bool shouldTrust,uint256 nonce)');
-	bytes32 public constant EXECUTE_HASH =
-		keccak256('Execute(address target,uint256 value,bytes payload,uint256 nonce)');
+    uint256 public nonce = 1;
+    uint256 public quorum;
+    bytes32 public immutable domainSeparator;
+    mapping(address => bool) public isSigner;
+    bytes32 public constant QUORUM_HASH =
+        keccak256("UpdateQuorum(uint256 newQuorum,uint256 nonce)");
+    bytes32 public constant SIGNER_HASH =
+        keccak256(
+            "UpdateSigner(address signer,bool shouldTrust,uint256 nonce)"
+        );
+    bytes32 public constant EXECUTE_HASH =
+        keccak256(
+            "Execute(address target,uint256 value,bytes payload,uint256 nonce)"
+        );
 
-	constructor(
-		string memory name,
-		address[] memory signers,
-		uint256 _quorum
-	) payable {
-		unchecked {
-			for (uint256 i = 0; i < signers.length; i++) isSigner[signers[i]] = true;
-		}
+    constructor(
+        string memory name,
+        address[] memory signers,
+        uint256 _quorum
+    ) payable {
+        unchecked {
+            for (uint256 i = 0; i < signers.length; i++)
+                isSigner[signers[i]] = true;
+        }
 
-		quorum = _quorum;
+        quorum = _quorum;
 
-		domainSeparator = keccak256(
-			abi.encode(
-				keccak256(
-					'EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'
-				),
-				keccak256(bytes(name)),
-				keccak256(bytes('1')),
-				block.chainid,
-				address(this)
-			)
-		);
-	}
+        domainSeparator = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                keccak256(bytes(name)),
+                keccak256(bytes("1")),
+                block.chainid,
+                address(this)
+            )
+        );
+    }
 
-	function execute(
-		address target,
-		uint256 value,
-		bytes calldata payload,
-		Signature[] calldata sigs
-	) public payable {
-		bytes32 digest = keccak256(
-			abi.encodePacked(
-				'\x19\x01',
-				domainSeparator,
-				keccak256(abi.encode(EXECUTE_HASH, target, value, payload, nonce++))
-			)
-		);
+    function execute(
+        address target,
+        uint256 value,
+        bytes calldata payload,
+        Signature[] calldata sigs
+    ) public payable {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainSeparator,
+                keccak256(
+                    abi.encode(EXECUTE_HASH, target, value, payload, nonce++)
+                )
+            )
+        );
 
-		address previous;
+        address previous;
 
-		unchecked {
-			for (uint256 i = 0; i < quorum; i++) {
-				address sigAddress = ecrecover(digest, sigs[i].v, sigs[i].r, sigs[i].s);
+        unchecked {
+            for (uint256 i = 0; i < quorum; i++) {
+                address sigAddress = ecrecover(
+                    digest,
+                    sigs[i].v,
+                    sigs[i].r,
+                    sigs[i].s
+                );
 
-				if (!isSigner[sigAddress] || previous >= sigAddress) revert InvalidSignatures();
+                if (!isSigner[sigAddress] || previous >= sigAddress)
+                    revert InvalidSignatures();
 
-				previous = sigAddress;
-			}
-		}
+                previous = sigAddress;
+            }
+        }
 
-		emit Executed(target, value, payload);
+        emit Executed(target, value, payload);
 
-		(bool success, ) = target.call{ value: value }(payload);
+        (bool success, ) = target.call{value: value}(payload);
 
-		if (!success) revert ExecutionFailed();
-	}
+        if (!success) revert ExecutionFailed();
+    }
 
-	function setQuorum(uint256 _quorum, Signature[] calldata sigs) public payable {
-		bytes32 digest = keccak256(
-			abi.encodePacked(
-				'\x19\x01',
-				domainSeparator,
-				keccak256(abi.encode(QUORUM_HASH, _quorum, nonce++))
-			)
-		);
+    function setQuorum(
+        uint256 _quorum,
+        Signature[] calldata sigs
+    ) public payable {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainSeparator,
+                keccak256(abi.encode(QUORUM_HASH, _quorum, nonce++))
+            )
+        );
 
-		address previous;
+        address previous;
 
-		unchecked {
-			for (uint256 i = 0; i < quorum; i++) {
-				address sigAddress = ecrecover(digest, sigs[i].v, sigs[i].r, sigs[i].s);
+        unchecked {
+            for (uint256 i = 0; i < quorum; i++) {
+                address sigAddress = ecrecover(
+                    digest,
+                    sigs[i].v,
+                    sigs[i].r,
+                    sigs[i].s
+                );
 
-				if (!isSigner[sigAddress] || previous >= sigAddress) revert InvalidSignatures();
+                if (!isSigner[sigAddress] || previous >= sigAddress)
+                    revert InvalidSignatures();
 
-				previous = sigAddress;
-			}
-		}
+                previous = sigAddress;
+            }
+        }
 
-		emit QuorumUpdated(_quorum);
+        emit QuorumUpdated(_quorum);
 
-		quorum = _quorum;
-	}
+        quorum = _quorum;
+    }
 
-	function setSigner(
-		address signer,
-		bool shouldTrust,
-		Signature[] calldata sigs
-	) public payable {
-		bytes32 digest = keccak256(
-			abi.encodePacked(
-				'\x19\x01',
-				domainSeparator,
-				keccak256(abi.encode(SIGNER_HASH, signer, shouldTrust, nonce++))
-			)
-		);
+    function setSigner(
+        address signer,
+        bool shouldTrust,
+        Signature[] calldata sigs
+    ) public payable {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainSeparator,
+                keccak256(abi.encode(SIGNER_HASH, signer, shouldTrust, nonce++))
+            )
+        );
 
-		address previous;
+        address previous;
 
-		unchecked {
-			for (uint256 i = 0; i < quorum; i++) {
-				address sigAddress = ecrecover(digest, sigs[i].v, sigs[i].r, sigs[i].s);
+        unchecked {
+            for (uint256 i = 0; i < quorum; i++) {
+                address sigAddress = ecrecover(
+                    digest,
+                    sigs[i].v,
+                    sigs[i].r,
+                    sigs[i].s
+                );
 
-				if (!isSigner[sigAddress] || previous >= sigAddress) revert InvalidSignatures();
+                if (!isSigner[sigAddress] || previous >= sigAddress)
+                    revert InvalidSignatures();
 
-				previous = sigAddress;
-			}
-		}
+                previous = sigAddress;
+            }
+        }
 
-		emit SignerUpdated(signer, shouldTrust);
+        emit SignerUpdated(signer, shouldTrust);
 
-		isSigner[signer] = shouldTrust;
-	}
+        isSigner[signer] = shouldTrust;
+    }
 
-	receive() external payable {}
+    receive() external payable {}
 }
--- chain/src/Superfluid.sol
+++ chain/src/Superfluid.sol
@@ -4,187 +4,199 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Superfluid {
-	error Unauthorized();
-	error StreamNotFound();
-	error StreamStillActive();
+    error Unauthorized();
+    error StreamNotFound();
+    error StreamStillActive();
 
-	event StreamCreated(Stream stream);
-	event StreamRefueled(uint256 indexed streamId, uint256 amount);
-	event FundsWithdrawn(uint256 indexed streamId, uint256 amount);
-	event ExcessWithdrawn(uint256 indexed streamId, uint256 amount);
-	event StreamDetailsUpdated(
-		uint256 indexed streamId,
-		uint256 paymentPerBlock,
-		Timeframe timeframe
-	);
+    event StreamCreated(Stream stream);
+    event StreamRefueled(uint256 indexed streamId, uint256 amount);
+    event FundsWithdrawn(uint256 indexed streamId, uint256 amount);
+    event ExcessWithdrawn(uint256 indexed streamId, uint256 amount);
+    event StreamDetailsUpdated(
+        uint256 indexed streamId,
+        uint256 paymentPerBlock,
+        Timeframe timeframe
+    );
 
-	struct Stream {
-		address sender;
-		address recipient;
-		ERC20 token;
-		uint256 balance;
-		uint256 withdrawnBalance;
-		uint256 paymentPerBlock;
-		Timeframe timeframe;
-	}
+    struct Stream {
+        address sender;
+        address recipient;
+        ERC20 token;
+        uint256 balance;
+        uint256 withdrawnBalance;
+        uint256 paymentPerBlock;
+        Timeframe timeframe;
+    }
 
-	struct Timeframe {
-		uint256 startBlock;
-		uint256 stopBlock;
-	}
+    struct Timeframe {
+        uint256 startBlock;
+        uint256 stopBlock;
+    }
 
-	struct Signature {
-		uint8 v;
-		bytes32 r;
-		bytes32 s;
-	}
+    struct Signature {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+    }
 
-	uint256 internal streamId = 1;
-	uint256 public nonce = 1;
-	bytes32 public immutable domainSeparator;
-	bytes32 public constant UPDATE_DETAILS_HASH =
-		keccak256(
-			'UpdateStreamDetails(uint256 streamId,uint256 paymentPerBlock,uint256 startBlock,uint256 stopBlock,uint256 nonce)'
-		);
-	mapping(uint256 => Stream) public getStream;
+    uint256 internal streamId = 1;
+    uint256 public nonce = 1;
+    bytes32 public immutable domainSeparator;
+    bytes32 public constant UPDATE_DETAILS_HASH =
+        keccak256(
+            "UpdateStreamDetails(uint256 streamId,uint256 paymentPerBlock,uint256 startBlock,uint256 stopBlock,uint256 nonce)"
+        );
+    mapping(uint256 => Stream) public getStream;
 
-	constructor() payable {
-		domainSeparator = keccak256(
-			abi.encode(
-				keccak256(
-					'EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'
-				),
-				keccak256(bytes('LilSuperfluid')),
-				keccak256(bytes('1')),
-				block.chainid,
-				address(this)
-			)
-		);
-	}
+    constructor() payable {
+        domainSeparator = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                keccak256(bytes("LilSuperfluid")),
+                keccak256(bytes("1")),
+                block.chainid,
+                address(this)
+            )
+        );
+    }
 
-	function streamTo(
-		address recipient,
-		ERC20 token,
-		uint256 initialBalance,
-		Timeframe memory timeframe,
-		uint256 paymentPerBlock
-	) external payable returns (uint256) {
-		Stream memory stream = Stream({
-			token: token,
-			sender: msg.sender,
-			withdrawnBalance: 0,
-			timeframe: timeframe,
-			recipient: recipient,
-			balance: initialBalance,
-			paymentPerBlock: paymentPerBlock
-		});
+    function streamTo(
+        address recipient,
+        ERC20 token,
+        uint256 initialBalance,
+        Timeframe memory timeframe,
+        uint256 paymentPerBlock
+    ) external payable returns (uint256) {
+        Stream memory stream = Stream({
+            token: token,
+            sender: msg.sender,
+            withdrawnBalance: 0,
+            timeframe: timeframe,
+            recipient: recipient,
+            balance: initialBalance,
+            paymentPerBlock: paymentPerBlock
+        });
 
-		emit StreamCreated(stream);
+        emit StreamCreated(stream);
 
-		getStream[streamId] = stream;
+        getStream[streamId] = stream;
 
-		token.transferFrom(msg.sender, address(this), initialBalance);
+        token.transferFrom(msg.sender, address(this), initialBalance);
 
-		return streamId++;
-	}
+        return streamId++;
+    }
 
-	function refuel(uint256 streamId, uint256 amount) public payable {
-		if (getStream[streamId].sender != msg.sender) revert Unauthorized();
+    function refuel(uint256 streamId, uint256 amount) public payable {
+        if (getStream[streamId].sender != msg.sender) revert Unauthorized();
 
-		unchecked {
-			getStream[streamId].balance += amount;
-		}
+        unchecked {
+            getStream[streamId].balance += amount;
+        }
 
-		emit StreamRefueled(streamId, amount);
+        emit StreamRefueled(streamId, amount);
 
-		getStream[streamId].token.transferFrom(msg.sender, address(this), amount);
-	}
+        getStream[streamId].token.transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+    }
 
-	function withdraw(uint256 streamId) public payable {
-		if (getStream[streamId].recipient != msg.sender) revert Unauthorized();
+    function withdraw(uint256 streamId) public payable {
+        if (getStream[streamId].recipient != msg.sender) revert Unauthorized();
 
-		uint256 balance = balanceOf(streamId, msg.sender);
+        uint256 balance = balanceOf(streamId, msg.sender);
 
-		unchecked {
-			getStream[streamId].withdrawnBalance += balance;
-		}
+        unchecked {
+            getStream[streamId].withdrawnBalance += balance;
+        }
 
-		emit FundsWithdrawn(streamId, balance);
+        emit FundsWithdrawn(streamId, balance);
 
-		getStream[streamId].token.transfer(msg.sender, balance);
-	}
+        getStream[streamId].token.transfer(msg.sender, balance);
+    }
 
-	function refund(uint256 streamId) public payable {
-		if (getStream[streamId].sender != msg.sender) revert Unauthorized();
-		if (getStream[streamId].timeframe.stopBlock > block.number) revert StreamStillActive();
+    function refund(uint256 streamId) public payable {
+        if (getStream[streamId].sender != msg.sender) revert Unauthorized();
+        if (getStream[streamId].timeframe.stopBlock > block.number)
+            revert StreamStillActive();
 
-		uint256 balance = balanceOf(streamId, msg.sender);
+        uint256 balance = balanceOf(streamId, msg.sender);
 
-		getStream[streamId].balance -= balance;
+        getStream[streamId].balance -= balance;
 
-		emit ExcessWithdrawn(streamId, balance);
+        emit ExcessWithdrawn(streamId, balance);
 
-		getStream[streamId].token.transfer(msg.sender, balance);
-	}
+        getStream[streamId].token.transfer(msg.sender, balance);
+    }
 
-	function calculateBlockDelta(Timeframe memory timeframe) internal view returns (uint256 delta) {
-		if (block.number <= timeframe.startBlock) return 0;
-		if (block.number < timeframe.stopBlock) return block.number - timeframe.startBlock;
+    function calculateBlockDelta(
+        Timeframe memory timeframe
+    ) internal view returns (uint256 delta) {
+        if (block.number <= timeframe.startBlock) return 0;
+        if (block.number < timeframe.stopBlock)
+            return block.number - timeframe.startBlock;
 
-		return timeframe.stopBlock - timeframe.startBlock;
-	}
+        return timeframe.stopBlock - timeframe.startBlock;
+    }
 
-	function balanceOf(uint256 streamId, address who) public view returns (uint256) {
-		Stream memory stream = getStream[streamId];
+    function balanceOf(
+        uint256 streamId,
+        address who
+    ) public view returns (uint256) {
+        Stream memory stream = getStream[streamId];
 
-		if (stream.sender == address(0)) revert StreamNotFound();
+        if (stream.sender == address(0)) revert StreamNotFound();
 
-		uint256 blockDelta = calculateBlockDelta(stream.timeframe);
-		uint256 recipientBalance = blockDelta * stream.paymentPerBlock;
+        uint256 blockDelta = calculateBlockDelta(stream.timeframe);
+        uint256 recipientBalance = blockDelta * stream.paymentPerBlock;
 
-		if (who == stream.recipient) return recipientBalance - stream.withdrawnBalance;
-		if (who == stream.sender) return stream.balance - recipientBalance;
+        if (who == stream.recipient)
+            return recipientBalance - stream.withdrawnBalance;
+        if (who == stream.sender) return stream.balance - recipientBalance;
 
-		return 0;
-	}
+        return 0;
+    }
 
-	function updateDetails(
-		uint256 streamId,
-		uint256 paymentPerBlock,
-		Timeframe memory timeframe,
-		Signature memory sig
-	) public payable {
-		Stream memory stream = getStream[streamId];
+    function updateDetails(
+        uint256 streamId,
+        uint256 paymentPerBlock,
+        Timeframe memory timeframe,
+        Signature memory sig
+    ) public payable {
+        Stream memory stream = getStream[streamId];
 
-		if (stream.sender == address(0)) revert StreamNotFound();
+        if (stream.sender == address(0)) revert StreamNotFound();
 
-		bytes32 digest = keccak256(
-			abi.encodePacked(
-				'\x19\x01',
-				domainSeparator,
-				keccak256(
-					abi.encode(
-						UPDATE_DETAILS_HASH,
-						streamId,
-						paymentPerBlock,
-						timeframe.startBlock,
-						timeframe.stopBlock,
-						nonce++
-					)
-				)
-			)
-		);
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainSeparator,
+                keccak256(
+                    abi.encode(
+                        UPDATE_DETAILS_HASH,
+                        streamId,
+                        paymentPerBlock,
+                        timeframe.startBlock,
+                        timeframe.stopBlock,
+                        nonce++
+                    )
+                )
+            )
+        );
 
-		address sigAddress = ecrecover(digest, sig.v, sig.r, sig.s);
+        address sigAddress = ecrecover(digest, sig.v, sig.r, sig.s);
 
-		if (
-			!(stream.sender == msg.sender && stream.recipient == sigAddress) &&
-			!(stream.sender == sigAddress && stream.recipient == msg.sender)
-		) revert Unauthorized();
+        if (
+            !(stream.sender == msg.sender && stream.recipient == sigAddress) &&
+            !(stream.sender == sigAddress && stream.recipient == msg.sender)
+        ) revert Unauthorized();
 
-		emit StreamDetailsUpdated(streamId, paymentPerBlock, timeframe);
+        emit StreamDetailsUpdated(streamId, paymentPerBlock, timeframe);
 
-		getStream[streamId].paymentPerBlock = paymentPerBlock;
-		getStream[streamId].timeframe = timeframe;
-	}
+        getStream[streamId].paymentPerBlock = paymentPerBlock;
+        getStream[streamId].timeframe = timeframe;
+    }
 }
