e7a487fea51c86dae06fcb64e5e65a74334b3e15
aleksey <azhlbn@gmail.com>
2023-02-18 20:38:42 +1000

    upd

--- client/scripts/SafeFactory.js
+++ client/scripts/SafeFactory.js
@@ -2,7 +2,7 @@ import provider from './provider'
 import { ethers } from 'ethers'
 import { abi } from '../../chain/out/SafeFactory.sol/SafeFactory.json'
 
-const contract_address = "0xf3da627aa64da49Ac0Bb95858eF745538A88588E"
+const contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 const SafeFactory = new ethers.Contract(contract_address, abi, provider)
 
--- client/scripts/defaultProvider.js
+++ client/scripts/defaultProvider.js
@@ -1,5 +1,20 @@
-import { ethers, InfuraProvider } from "ethers";
+// import { ethers, InfuraProvider } from "ethers";
 
-const defaultProvider = new ethers.providers.InfuraProvider();
+// const defaultProvider = new ethers.providers.InfuraProvider();
+
+// export default defaultProvider;
+
+import { ethers } from "ethers";
+
+let provider;
+
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+    defaultProvider = new ethers.providers.Web3Provider(window.ethereum);
+} else {
+    defaultProvider = new ethers.providers.JsonRpcProvider(
+    "https://bsc-dataseed.binance.org/"
+  );
+  // provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed.binance.org/");
+}
 
 export default defaultProvider;
