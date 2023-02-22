ae10cbe69dd0a40b9b316d7c0b763fd7bf243174
lekaus <zasemozer@gmail.com>
2023-02-22 04:08:53 +0300

    sendstr and some dashbrd

--- client/pages/sendstream.js
+++ client/pages/sendstream.js
@@ -3,80 +3,85 @@ import Layout from "../components/layout";
 const Sendstream = () => {
   return (
     <Layout>
-      <div>
-        <div className="bg-white shadow-md rounded-[20px] px-8 pt-6 pb-8 mb-4 flex flex-col mx-auto mt-40">
-          <div className="-mx-3 md:flex mb-6">
-            <div className="md:w-full px-3">
-              <label
-                className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                for="grid-password"
-              >
-                Receiver Wallet Address
-              </label>
-              <input
-                className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3"
-                id="grid-password"
-                type="password"
-                placeholder="Public Address or ENS"
-              />
-            </div>
+      <div className="create-new-safe">
+      <div className="grid-container">
+          <div className="MuiGrid-item grid-item">
+            <h2 className="title">Send stream</h2>
           </div>
-          <div className="-mx-3 md:flex mb-6">
-            <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-              <label
-                for="super-token"
-                className="block mb-2 text-sm font-bold text-gray-900 dark:text-gray-400"
-              >
-                Super Token
-              </label>
-              <select
-                id="super-token"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              >
-                <option selected>Select a token</option>
-                <option value="US">ETHx</option>
-                <option value="CA">fUSDCx</option>
-                <option value="FR">fTUCDx</option>
-                <option value="DE">fDaIx</option>
-              </select>
-            </div>
-            <div className="md:w-1/2 px-3">
-              <label
-                for="flow-rate"
-                className="block mb-2 text-sm font-bold text-gray-900 dark:text-gray-400"
-              >
-                Flow Rate
-              </label>
-              <input
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                // id="grid-last-name"
-                type="text"
-                placeholder="0.0"
-              />
-              <select
-                id="flow-rate"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              >
-                {/* <option selected>/ second</option> */}
-                <option value="sec">/ second</option>
-                <option value="min">/ minute</option>
-                <option value="hour">/ hour</option>
-                <option value="day">/ day</option>
-                <option value="month">/ month</option>
-                <option value="year">/ year</option>
-              </select>
-            </div>
+          <div className="MuiGrid-item grid-item">
+          <div className="card">
+          <div className="progress-bar">
+                <span className="linear-progress-bar">
+                  <span className="progress-bar-fill"></span>
+                </span>
           </div>
-          <button
-            type="button"
-            data-mdb-ripple="true"
-            data-mdb-ripple-color="light"
-            className="flex px-6 py-2.5 bg-[#6671FFCC] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          >
-            Send stream
-          </button>
+              <span className="card-title">
+              Select a receiver
+              </span>
+              <div className="wallets">
+                <form>
+                  <div className="wallets-name">
+                    <label>Receiver Wallet Address:</label>
+                    <input type="text" placeholder='Public Address or ENS' />
+                  </div>
+
+                  <div className="mx-3 md:flex mb-6">
+                  <div className="mt-10 md:w-1/2 px-3 mb-6 md:mb-0">                  
+                        <label>Super Token</label>
+                        <select
+                          // id="super-token"
+                          className="mt-2 bg-gray-50 border border-gray-300 text-gray-500 text-base focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        >
+                          <option selected>Select a token</option>
+                          <option value="US">ETHx</option>
+                          <option value="CA">fUSDCx</option>
+                          <option value="FR">fTUCDx</option>
+                          <option value="DE">fDaIx</option>
+                        </select>
+                  </div>
+                  
+                    <div className="mt-10 md:w-1/2 px-3">
+                    <div className="wallets-name">
+                      <label>Flow Rate</label>
+                      <input
+                        className="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-base focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        // id=""
+                        type="text"
+                        placeholder="0.0"
+                      />                      
+                      
+                      <select
+                        // id="flow-rate"
+                        className="bg-gray-50 border border-gray-300 text-gray-500 text-base focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      >                      
+                        <option value="sec">/ second</option>
+                        <option value="min">/ minute</option>
+                        <option value="hour">/ hour</option>
+                        <option value="day">/ day</option>
+                        <option value="month">/ month</option>
+                        <option value="year">/ year</option>
+                      </select>
+                      </div>
+                    </div>
+                  </div>
+
+                  <hr className="divider" />
+                  <div className="input-row">
+                    <div className="button-container">
+                      <button className="submit-button" type="submit">
+                        Send stream
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+          
+          
+        </div>
+        </div>
         </div>
       </div>
+
     </Layout>
   );
 };
