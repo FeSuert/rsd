2db98a653283182fb2c10eb78b78413bb832193f
Ivan Urdenko <vaniaurdenko@gmail.com>
2023-02-24 23:13:59 +0100

    changed wallets and create safe pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/sidebar.css'
 import '../styles/safes.css'
 import '../styles/wallets.css'
 import '../styles/mainpage.css'
+import '../styles/lds_roller.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
