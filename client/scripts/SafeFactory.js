0345fe13cf6918b6e94a3944892cebab2557aa48
aleksey <azhlbn@gmail.com>
2023-02-26 01:35:54 +1000

    upd

--- client/scripts/SafeFactory.js
+++ client/scripts/SafeFactory.js
@@ -2,7 +2,7 @@ import provider from './provider'
 import { ethers } from 'ethers'
 import { abi } from '../../chain/out/SafeFactory.sol/SafeFactory.json'
 
-const contract_address = "0xf174a20453059b07b3fc36b42a4a43b4bcca2a70"
+const contract_address = "0xE0b9F30D281e3339B20299aB1761c7C8f1e11D54"
 
 const SafeFactory = new ethers.Contract(contract_address, abi, provider)
 
