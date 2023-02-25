0418e41936c2ea8638ca5d96856ae48f05c947d2
Yuri.P <domosedov@mail.ru>
2023-02-25 12:26:39 +0300

    context_update

--- client/components/layout.js
+++ client/components/layout.js
@@ -1,19 +1,13 @@
-import Button from "./Button";
-import NavPanel from "./nav_panel";
 import SideBar from "./sidebar";
-import { Connect } from "./Connect";
 import ChainName from "./chainname";
 
-const Layout = ({ children, chainIdName }) => {
+const Layout = ({ children }) => {
   return (
     <div className="styles_content__Vufwi">
       <SideBar className="absolute" />
       <div className="flex flex-row container justify-center items-start	max-w-screen-xl mx-auto">
         {children}
       </div>
-      {/* <button type="button" className="network-button">
-        Network {chainIdName}
-      </button> */}
       <ChainName />
     </div>
   );
